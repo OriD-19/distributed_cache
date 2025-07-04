fcf003c5961eafb96e3cb76bd51e3536f028e3a3
Fernando Fuentes <fernandocastillo.8@hotmail.com>
2025-07-03 21:41:33 -0600

    Consistent Hashing creation methods

--- consistent_hashing/consistentHashing.go
+++ consistent_hashing/consistentHashing.go
@@ -16,9 +16,29 @@ type CacheNode struct {
 	CacheClient *lruCache.LRUCache
 }
 
-type ConsistentHashing struct {
-	HashRing map[int]CacheNode
+// Consistent Hashing implementation
+type HashRing struct {
+	Ring map[int]*CacheNode
 	Nodes    []int // sorted list with all the hashes of nodes
 }
 
-func NewCacheNode(cache *lruCache.LRUCache)
+func NewCacheNode(cache *lruCache.LRUCache, id string) *CacheNode {
+	
+	var cacheNode CacheNode
+
+	cacheNode.Status = RUNNING // running node by default
+	cacheNode.CacheClient = cache
+	cacheNode.ID = id
+
+	return &cacheNode
+}
+
+func NewHashRing() *HashRing {
+
+	var hashRing HashRing	
+
+	hashRing.Ring = make(map[int]*CacheNode)
+	hashRing.Nodes = []int{}
+
+	return &hashRing
+}
